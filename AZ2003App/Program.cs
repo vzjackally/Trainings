540d648f73c6e978995b026d003162035752d7a4
Vladislav_Zaitcev <Vladislav_Zaitcev@epam.com>
2025-01-30 19:48:32 +0100

    55555

--- AZ2003App/Program.cs
+++ AZ2003App/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World 1112223334444!");
+app.MapGet("/", () => "Hello World 111222333444455555!");
 
 app.Run();
