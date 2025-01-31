4bb16f3122b4621ad8ad6e93126bbdd982023b2f
Vladislav_Zaitcev <Vladislav_Zaitcev@epam.com>
2025-01-31 18:48:54 +0100

    significant code change

--- AZ2003App/Program.cs
+++ AZ2003App/Program.cs
@@ -1,6 +1,34 @@
+using System.Net;
+using System.Net.Sockets;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World 11122233344445555566!");
+app.MapGet("/", async () =>
+{
+    // Get the local (container) IPv4 address
+    var hostName = Dns.GetHostName();
+    var hostEntry = await Dns.GetHostEntryAsync(hostName);
+    var localIp = hostEntry.AddressList
+        .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork)
+        ?.ToString() ?? "Not found";
+
+    // Get the WAN/Public IP by calling a simple external IP service
+    using var httpClient = new HttpClient();
+    string wanIp;
+    try
+    {
+        // api.ipify.org returns the IP as plain text
+        // Alternatives: ifconfig.me, checkip.amazonaws.com, etc.
+        wanIp = await httpClient.GetStringAsync("https://api.ipify.org");
+    }
+    catch
+    {
+        wanIp = "Unable to retrieve public IP";
+    }
+
+    // Return a response showing both IPs
+    return $"Hello from .NET!\nLocal IP: {localIp}\nPublic IP: {wanIp}";
+});
 
 app.Run();
