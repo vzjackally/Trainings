6883327cf15fc1da43f9716bde1bd26d0c0aee69
Vladislav_Zaitcev <Vladislav_Zaitcev@epam.com>
2025-02-04 11:26:58 +0100

    minor fixes

--- AZ2003App/Program.cs
+++ AZ2003App/Program.cs
@@ -1,50 +1,56 @@
 using System.Net;
 using System.Net.Sockets;
 
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", async () =>
+internal class Program
 {
-    // Get the local (container) IPv4 address
-    var hostName = Dns.GetHostName();
-    var hostEntry = await Dns.GetHostEntryAsync(hostName);
-    var localIp = hostEntry.AddressList
-        .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork)
-        ?.ToString() ?? "Not found";
-
-    // Get the WAN/Public IP by calling a simple external IP service
-    using var httpClient = new HttpClient();
-    string wanIp;
-    try
+    private static void Main(string[] args)
     {
-        // api.ipify.org returns the IP as plain text
-        // Alternatives: ifconfig.me, checkip.amazonaws.com, etc.
-        wanIp = await httpClient.GetStringAsync("https://api.ipify.org");
-    }
-    catch
-    {
-        wanIp = "Unable to retrieve public IP";
-    }
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
 
-    // Return a response showing both IPs
-    var ipListBuilder = new System.Text.StringBuilder();
+        app.MapGet("/", async () =>
+        {
+            // Get the local (container) IPv4 address
+            var hostName = Dns.GetHostName();
+            var hostEntry = await Dns.GetHostEntryAsync(hostName);
+            var localIp = hostEntry.AddressList
+                .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork)
+                ?.ToString() ?? "Not found";
 
-    if (hostEntry?.AddressList == null)
-        return "No IP addresses found";
+            // Get the WAN/Public IP by calling a simple external IP service
+            using var httpClient = new HttpClient();
+            string wanIp;
+            try
+            {
+                // api.ipify.org returns the IP as plain text
+                // Alternatives: ifconfig.me, checkip.amazonaws.com, etc.
+                wanIp = await httpClient.GetStringAsync("https://api.ipify.org");
+            }
+            catch
+            {
+                wanIp = "Unable to retrieve public IP";
+            }
 
-    hostEntry.AddressList
-        .Where(ip => ip.AddressFamily == AddressFamily.InterNetwork)
-        .ToList()
-        .ForEach(ip => ipListBuilder.AppendLine(ip.ToString()));
+            // Return a response showing both IPs
+            var ipListBuilder = new System.Text.StringBuilder();
 
-    return $"""
+            if (hostEntry?.AddressList == null)
+                return "No IP addresses found";
+
+            hostEntry.AddressList
+                .Where(ip => ip.AddressFamily == AddressFamily.InterNetwork)
+                .ToList()
+                .ForEach(ip => ipListBuilder.AppendLine(ip.ToString()));
+
+            return $"""
     Hello from .NET!!!!!
     Local IP: {localIp}
     Public IP: {wanIp}
     Other IPs:
     {ipListBuilder}
     """;
-});
+        });
 
-app.Run();
+        app.Run();
+    }
+}
